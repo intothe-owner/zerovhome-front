9069d7fb024faa47719a1bac8be51028defd005d
syconsulting2026-glitch <syconsulting2026@gmail.com>
2026-08-31 20:08:38 +0900

    next.config 수정

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'zerovapp-bucket.s3.ap-northeast-2.amazonaws.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
